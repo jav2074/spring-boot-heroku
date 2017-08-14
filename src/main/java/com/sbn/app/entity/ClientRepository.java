18f9023b8b392166cc9807ca5eafdb9411a97719
jav.electrochance <jav.electrochance@gmail.com>
2017-08-13 22:52:10 -0300

    Add clientRepository.exists

--- src/main/java/com/sbn/app/entity/ClientRepository.java
+++ src/main/java/com/sbn/app/entity/ClientRepository.java
@@ -4,10 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-//@Repository
-//public interface ClientRepository extends JpaRepository <Client, Long> {
-//}
-
 @Repository
-public interface ClientRepository extends CrudRepository <Client, Long> {
+public interface ClientRepository extends JpaRepository <Client, Long> {
 }
+
+//@Repository
+//public interface ClientRepository extends CrudRepository <Client, Long> {
+//}
