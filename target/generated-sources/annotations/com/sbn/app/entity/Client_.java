4b2d73f1f310de1bbcfdd1c2bd4c18d74e136fd5
jav2074 <jav.electrochance@gmail.com>
2020-04-06 23:20:37 -0300

    Add NetBeans

---
+++ target/generated-sources/annotations/com/sbn/app/entity/Client_.java
@@ -0,0 +1,18 @@
+package com.sbn.app.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-04-06T23:16:39")
+@StaticMetamodel(Client.class)
+public class Client_ { 
+
+    public static volatile SingularAttribute<Client, String> address;
+    public static volatile SingularAttribute<Client, String> phone;
+    public static volatile SingularAttribute<Client, String> name;
+    public static volatile SingularAttribute<Client, Long> id;
+    public static volatile SingularAttribute<Client, String> email;
+    public static volatile SingularAttribute<Client, String> lastname;
+
+}
---
+++ target/generated-sources/annotations/com/sbn/app/entity/Product_.java
@@ -0,0 +1,16 @@
+package com.sbn.app.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-04-06T23:16:39")
+@StaticMetamodel(Product.class)
+public class Product_ { 
+
+    public static volatile SingularAttribute<Product, Integer> quantity;
+    public static volatile SingularAttribute<Product, Double> price;
+    public static volatile SingularAttribute<Product, String> name;
+    public static volatile SingularAttribute<Product, Long> id;
+
+}
